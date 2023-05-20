eee90d9fdff304dccf46db12e38b6f8bb04b7e61
Digonil <diego.niles@hotmail.com>
2023-05-20 11:56:21 -0300

    Criado a entidade de pedido (Order)

---
+++ src/main/java/com/niles/projetosistemadepagamento/entitities/Order.java
@@ -0,0 +1,50 @@
+package com.niles.projetosistemadepagamento.entitities;
+
+public class Order {
+
+    private Integer orderId;
+    private Double basic;
+    private Double discount;
+
+    public Order() {
+    }
+
+    public Order(Integer orderId, Double basic, Double discount) {
+        this.orderId = orderId;
+        this.basic = basic;
+        this.discount = discount;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Double getBasic() {
+        return basic;
+    }
+
+    public void setBasic(Double basic) {
+        this.basic = basic;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", basic=" + basic +
+                ", discount=" + discount +
+                '}';
+    }
+}
