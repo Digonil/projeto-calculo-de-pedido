be7263f08cf086c10386cb710994812706c783a4
Digonil <diego.niles@hotmail.com>
2023-05-20 11:52:38 -0300

    First commit

---
+++ src/main/java/com/niles/projetosistemadepagamento/ProjetoSistemaDePagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.niles.projetosistemadepagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSistemaDePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSistemaDePagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niles/projetosistemadepagamento/ProjetoSistemaDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niles.projetosistemadepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSistemaDePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
